0b646c83970b7fe73fe01e54c9fd4df6f9584d30
Jake Reimer <jreimer@at-compute001.bcm.tmc.edu>
2013-09-25 14:50:41 -0500

    Changed "key" field to "error_key"

--- schemas/+reso/Jobs.m
+++ schemas/+reso/Jobs.m
@@ -6,7 +6,7 @@ table_name : varchar(255) # className of the table
 key_hash   : char(32)     # key hash
 -----
 status    : enum("reserved","error","ignore") # if tuple is missing, the job is available
-key=null           : blob                              # structure containing the key
+error_key=null           : blob                              # structure containing the key
 error_message=""   : varchar(1023)                     # error message returned if failed
 error_stack=null   : blob                              # error stack if failed
 host=""            : varchar(255)                      # system hostname
