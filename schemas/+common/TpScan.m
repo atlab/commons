4c351b8f1ff0c3ab1f8722f955ffcc71739edf82
jakereimer <jreimer@cns.bcm.edu>
2014-07-12 21:39:56 -0500

    Added fields to TpScan and TpSession

--- schemas/+common/TpScan.m
+++ schemas/+common/TpScan.m
@@ -3,10 +3,12 @@ common.TpScan (manual) # scanimage scan info
 ->common.TpSession
 scan_idx : smallint # scanimage-generated sequential number
 -----
-surfz  : float   # (um) z-coord at pial surface
-laser_wavelength  : float # (nm)
-laser_power :  float # (mW) to brain
-scan_notes = "" : varchar(4095)  #  free-notes
+surfz               : float   # (um) z-coord at pial surface
+depth=0             : int     # manual depth measurement 
+laser_wavelength    : float # (nm)
+laser_power         : float # (mW) to brain
+cortical_area="V1"  : enum('other','unknown','V1','LM','AL','PM') # Location of scan
+scan_notes = ""     : varchar(4095)  #  free-notes
 scan_ts = CURRENT_TIMESTAMP : timestamp   # don't edit
 %}
 
--- schemas/+common/TpSession.m
+++ schemas/+common/TpSession.m
@@ -10,6 +10,7 @@ basename="scan"             : varchar(80)                   # scanimage base fil
 lens                        : enum('4x','10x','16x','20x','25x','40x','60x')# objective lens
 fov                         : float                         # (um) across the full FOV at mag = 1.0
 fluorophore="Unknown"       : enum('OGB','GCaMP6f','GCaMP6m','GCaMP6s','Unknown','GCaMP3')# 
+compartment="unspecified"   : enum('unspecified','soma','dendrites','axons') # Imaging target
 anesthesia="other"          : enum('isoflurane','fentanyl','urethane','awake','other')# per protocol
 tp_session_notes=""         : varchar(4095)                 # free-text notes
 tp_ts=CURRENT_TIMESTAMP     : timestamp                     # automatic
