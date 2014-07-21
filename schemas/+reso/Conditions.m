b8c27f8e394cb818b23dcaf454fb8460918aef53
Jake Reimer <jreimer@at-compute001.bcm.tmc.edu>
2014-07-20 23:49:43 -0500

    removed poprel from reso.Conditions

--- schemas/+reso/Conditions.m
+++ schemas/+reso/Conditions.m
@@ -1,9 +1,8 @@
 %{
-reso.Conditions (lookup)            # conditions to use for reso.Indicator
+reso.Conditions (lookup)            # conditions to use for reso.Indicator ** MUST UPDATE popRel in reso.Indicator for new conditions **
 condition_num  : tinyint            # condition number
 -----
 condition_name          : varchar(64)   # name of condition
-condition_pop_str       : varchar(255)  # restricts populate to reso.Align & reso.VolumeSlice & eval(condition_pop_str)
 condition_params=NULL   : mediumblob    # parameters for reso.Indicator
 
 %}
@@ -22,22 +21,18 @@ classdef Conditions < dj.Relvar
         function fill(self)
             tuples(1).condition_num=1;
             tuples(1).condition_name = 'quiet dilating';
-            tuples(1).condition_pop_str = 'pro(patch.Eye & patch.EyeFrame,''file_num->scan_idx'',''session->tp_session'')';
             tuples(1).condition_params = [];
 
             tuples(2).condition_num=2;
             tuples(2).condition_name = 'quiet constricting';
-            tuples(2).condition_pop_str = 'pro(patch.Eye & patch.EyeFrame,''file_num->scan_idx'',''session->tp_session'')';
             tuples(2).condition_params = [];
             
             tuples(3).condition_num=3;
             tuples(3).condition_name = 'quiet';
-            tuples(3).condition_pop_str = 'pro(patch.Recording & patch.Running,''file_num->scan_idx'',''session->tp_session'')';
             tuples(3).condition_params = [];
 
             tuples(4).condition_num=4;
             tuples(4).condition_name = 'active';
-            tuples(4).condition_pop_str = 'pro(patch.Recording & patch.Running,''file_num->scan_idx'',''session->tp_session'')';
             tuples(4).condition_params = [];
 
             self.inserti(tuples)            
