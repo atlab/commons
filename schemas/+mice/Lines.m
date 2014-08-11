1afae8534de5293123657bc0bbd398ba99467c27
cathryn1 <crhughes@bcm.edu>
2014-08-11 13:56:26 -0500

    Corrections to submitLine.m

--- schemas/+mice/Lines.m
+++ schemas/+mice/Lines.m
@@ -1,13 +1,13 @@
 %{
-mice.Lines (manual) # Basic mouse line info
-line            : enum('Nestin-Cre','Nestin-CreER(W)','Nestin-CreER(J)','Wfs1-CreER','ChAT-Cre','Viaat-Cre','SST-Cre','PV-Cre','VIP-Cre','GAD67-GFP','KOPRCS','Ai9','R-EYFP','R-ChR2-EYFP','R-ChR2-tdTomato','R-TVA/G','R-Arch','R-GCaMP3','Confetti','Nuc4','Nuc24','Cyt47','C57Bl/6','Fvb','Etv1-CreER')# Mouse Line Abbreviation
+mice.Lines (manual) # Basic mouse line info$
+line            : enum('Nestin-Cre','Nestin-CreER(W)','Nestin-CreER(J)','Wfs1-CreER','ChAT-Cre','Viaat-Cre','SST-Cre','PV-Cre','VIP-Cre','GAD67-GFP','KOPRCS','Ai9','R-EYFP','R-ChR2-EYFP','R-ChR2-tdTomato','R-TVA/G','R-Arch','R-GCaMP3','Confetti','Nuc4','Nuc24','Cyt47','C57Bl/6','Fvb','Etv1-CreER')  # Mouse Line Abbreviation
 ---
-line_full=""                : varchar(100)                  # full line name
-rec_strain=""               : varchar(20)                   # recipient strain
-donor_strain=""             : varchar(20)                   # donor strain
+line_full                   : varchar(100)                  # full line name
+rec_strain                  : varchar(20)                   # recipient strain
+donor_strain                : varchar(20)                   # donor strain
 n=null                      : tinyint                       # minimumm number of backcrosses to recipient strain
-seq=""                      : varchar(5000)                 # sequence of transgene, if available
-line_notes=""               : varchar(4096)                 # other comments
+seq                         : varchar(5000)                 # sequence of transgene, if available
+line_notes                  : varchar(4096)                 # other comments
 line_ts=CURRENT_TIMESTAMP   : timestamp                     # automatic
 %}
 
