2e661971eb6313f7b11ba0d8a53696595240058c
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2015-12-04 20:07:06 -0600

    debugged VanGogh stimulus

--- schemas/+psy/VanGoghLookup.m
+++ schemas/+psy/VanGoghLookup.m
@@ -17,26 +17,26 @@ classdef VanGoghLookup < dj.Relvar
     end
     
     methods
-        function [m, key] = lookup(self, cond, degxy, fps)
+        function [movie, key] = lookup(self, cond, degxy, fps)
             % make noise stimulus movie  and update condition
             % INPUTS:
             %   cond  - condition parameters
             %   degxy - visual degrees across x and y
             %   fps   - frames per second
             
-            key.vangogh_version = 1;  % increment if you make any changes to the code below
+            key.vangogh_version = 2;  % increment if you make any changes to the code below
             
             params = {cond degxy fps};
             hash = dj.DataHash(params);
             key.vangogh_paramhash = hash(1:10);
             
             if count(psy.VanGoghLookup & key)
-                m = fetch1(self & key, 'movie');
+                movie = fetch1(self & key, 'movie');
             else
                 % create gaussian movie
                 [movie, ori, kappa, contrast] = vangoghnoise(cond, degxy, fps);
-                
-                tuple.params = [params {stim}];
+                tuple = key;
+                tuple.params = params;
                 tuple.movie = movie;
                 tuple.kappa = kappa;
                 tuple.ori = ori;
