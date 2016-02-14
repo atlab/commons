0470594d1c4f3aa35c7144e2d043168e4ce948ee
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2016-02-13 22:19:45 -0600

    bugfix in ne7.ip.correctMotion (introduced in last commit)

--- lib/+ne7/+ip/correctMotion.m
+++ lib/+ne7/+ip/correctMotion.m
@@ -3,6 +3,7 @@ function img = correctMotion(img, xymotion)
 % xy motion: motion correct in x and y with subpixel precision
 
 assert(ismatrix(img) && numel(xymotion)==2, 'cannot correct stacks. Only 2D images please')
+sz = size(img);
 g = griddedInterpolant(img,'cubic','nearest');   % handles boundaries
 [y,x] = ndgrid((1:sz(1))+xymotion(2), (1:sz(2))+xymotion(1)); 
 img = g(y,x);
