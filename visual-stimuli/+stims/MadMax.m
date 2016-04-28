a21a98addbc0ad82838917ed3e9cf55d12e215b1
Edgar Y. Walker <edgar.walker@gmail.com>
2016-04-27 20:16:01 -0500

    Fixes to MadMax stim, correct frame rate, etc.

--- visual-stimuli/+stims/MadMax.m
+++ visual-stimuli/+stims/MadMax.m
@@ -3,7 +3,7 @@ classdef MadMax < stims.core.Visual
         function showTrial(self, cond)       
             moviename = sprintf(cond.path_template, cond.clip_number);
             movie = Screen('OpenMovie', self.win, moviename);
-            Screen('PlayMovie', movie, 1);            
+            Screen('PlayMovie', movie,1);            
             for i=1:ceil(cond.cut_after*self.screen.fps)
                 if self.screen.escape, break, end
                 tex = Screen('GetMovieImage', self.win, movie);
@@ -14,6 +14,7 @@ classdef MadMax < stims.core.Visual
                 self.screen.flip(false, false, i==1)
                 Screen('close',tex)
             end
+            Screen('CloseMovie',movie)
         end
     end
 end
