8bf37dce7c963a1f04e2179877a5fc87b2e69dd4
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-05-05 20:48:20 -0500

    added psy.MadMaxStills for storing static frames from the Mad Max movie

--- visual-stimuli/+stims/MadMax.m
+++ visual-stimuli/+stims/MadMax.m
@@ -2,6 +2,7 @@ classdef MadMax < stims.core.Visual
     methods
         function showTrial(self, cond)       
             moviename = sprintf(cond.path_template, cond.clip_number);
+            disp(moviename)
             movie = Screen('OpenMovie', self.win, moviename);
             Screen('PlayMovie', movie,1);            
             for i=1:ceil(cond.cut_after*self.screen.fps)
