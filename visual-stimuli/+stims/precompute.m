d151cb67c1ee4e6bcdc21c0a5d0bd73cb0a120c2
Fabian Sinz <fabee@epagoge.de>
2016-05-19 13:56:05 -0500

    remove visual-stimuli

--- visual-stimuli/+stims/precompute.m
+++
@@ -1,9 +0,0 @@
-function precompute(menu)
-% simulates running all the protocols, triggering precomputation of conditions
-
-for protocol = menu(:)'
-    fprintf('Protocol: %s:\n', protocol.constants.stimulus)
-    for i=1:length(protocol.stim)
-        init(protocol.stim{i}, [], protocol.constants);
-    end
-end
--- visual-stimuli/README.txt
+++
@@ -1,3 +0,0 @@
-This simple visual stimulus program works with DataJoint 2.0+ to log stimulus data. 
-
-Synchronization is performed using coded photodiode flips.
