b1aae7f269b37b27b89cb0aef71f8fcd6c2ad777
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-07-06 15:33:24 -0500

    bugfix in raster correction

--- lib/+ne7/+ip/correctRaster.m
+++ lib/+ne7/+ip/correctRaster.m
@@ -6,12 +6,11 @@ function img = correctRaster(img, rasterPhase, fillFraction)
 % img size [x y nSlices nFrames].   2D, 3D, 4D images also work
 
 assert(ndims(img)<=5)
-sz = size(img);
-ix = (-sz(2)/2+0.5:sz(2)/2-0.5)/(sz(2)/2);
+ix = (-size(img, 2)/2+0.5:size(img, 2)/2-0.5)/(size(img, 2)/2);
 tx = asin(ix*fillFraction);  % convert index to time
-for iChannel = 1:sz(3)
-    for iSlice = 1:sz(4)
-        for iFrame = 1:sz(5)
+for iChannel = 1:size(img, 3)
+    for iSlice = 1:size(img, 4)
+        for iFrame = 1:size(img, 5)
             im = img(:,:,iChannel, iSlice, iFrame);
             extrapVal = mean(im(:));
             img(1:2:end, :, iChannel, iSlice, iFrame) = interp1(ix, im(1:2:end,:)', ...
