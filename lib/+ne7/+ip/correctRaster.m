9228453df03ece0ef091964afb0e9b3bb6d28f20
dimitri-yatsenko <dimitri.yatsenko@gmail.com>
2016-07-06 15:29:45 -0500

    fixed bug in raster correction

--- lib/+ne7/+ip/correctRaster.m
+++ lib/+ne7/+ip/correctRaster.m
@@ -1,22 +1,24 @@
 function img = correctRaster(img, rasterPhase, fillFraction)
-% raster correction for resonant scanners.  
+% raster correction for resonant scanners.
 % rasterPhase is the phase different between left-right and right-left scan
-% lines. 
+% lines.
 %
 % img size [x y nSlices nFrames].   2D, 3D, 4D images also work
 
-
-sz = [size(img,1) size(img,2) size(img,3) size(img,4)];
+assert(ndims(img)<=5)
+sz = size(img);
 ix = (-sz(2)/2+0.5:sz(2)/2-0.5)/(sz(2)/2);
 tx = asin(ix*fillFraction);  % convert index to time
-for iSlice = 1:size(img,3)
-    for iFrame = 1:size(img, 4)
-        im = img(:,:,iSlice,iFrame);
-        extrapVal = mean(im(:));
-        img(1:2:end,:,iSlice,iFrame) = interp1(ix, im(1:2:end,:)', ...
-            sin(tx'+rasterPhase)/fillFraction,'linear',extrapVal)';
-        img(2:2:end,:,iSlice,iFrame) = interp1(ix, im(2:2:end,:)', ...
-            sin(tx'-rasterPhase)/fillFraction,'linear',extrapVal)';
+for iChannel = 1:sz(3)
+    for iSlice = 1:sz(4)
+        for iFrame = 1:sz(5)
+            im = img(:,:,iChannel, iSlice, iFrame);
+            extrapVal = mean(im(:));
+            img(1:2:end, :, iChannel, iSlice, iFrame) = interp1(ix, im(1:2:end,:)', ...
+                sin(tx'+rasterPhase)/fillFraction,'linear',extrapVal)';
+            img(2:2:end, :, iChannel, iSlice, iFrame) = interp1(ix, im(2:2:end,:)', ...
+                sin(tx'-rasterPhase)/fillFraction,'linear',extrapVal)';
+        end
     end
 end
 end
