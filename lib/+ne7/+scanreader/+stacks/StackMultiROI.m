6d778cf94038d945b30b7bf9e75e24850007040d
ecobost <emcobost@gmail.com>
2017-09-21 20:07:06 -0500

    fix reading multiroi stacks

--- lib/+ne7/+scanreader/+stacks/StackMultiROI.m
+++ lib/+ne7/+scanreader/+stacks/StackMultiROI.m
@@ -1,2 +1,8 @@
 classdef StackMultiROI < ne7.scanreader.scans.ScanMultiROI & ne7.scanreader.stacks.BaseStack
+    methods
+        function obj = StackMultiROI(joinContiguous)
+            % make sure the right constructor is called
+            obj@ne7.scanreader.scans.ScanMultiROI(joinContiguous)
+        end
+    end
 end
